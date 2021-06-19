334b3bbbc5f98d45846e1f7b0143e2d7f65776ed
rianbrug <riancvb@gmail.com>
2021-06-19 18:26:53 -0300

    company name to df

--- app.py
+++ app.py
@@ -16,12 +16,16 @@ with open(filepath) as fp:
     cnt = 1
     while line:
         # print(" ISSO EH PRINT ? ")
-        if(int(line[7])==0):
+        if(int(line[7]) == 0):
             line_columns = line.split()
-            df = pd.DataFrame([line_columns], columns=["Nome da Empresa", "Num de Inscrição da Empresa", "Nome do Banco", "Nome da Rua", "Número do Local", "Nome da Cidade", "CEP"])
-            print(df)
-            # print("Line {}: {}".format(cnt, line.strip()))
-            # print(" POS 8 ", line[7], "\n")
-            line = fp.readline()
-            # df_header =
-            cnt += 1
+            nameCompany1 = ''.join([i for i in line_columns[2] if not i.isdigit()])
+            nameCompany = [nameCompany1 + " " + line_columns[3]]
+            df = pd.DataFrame(
+                            columns=["Nome da Empresa",
+                            "Num de Inscrição da Empresa", "Nome do Banco",
+                            "Nome da Rua", "Número do Local",
+                            "Nome da Cidade", "CEP", "UF"],
+                            )
+            df["Nome da Empresa"] = nameCompany
+        line = fp.readline()
+        cnt += 1
