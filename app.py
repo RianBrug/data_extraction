111d9db36d7235278a12704af8b1ed7dc1352df0
rianbrug <riancvb@gmail.com>
2021-06-19 16:55:45 -0300

    adjust tab size to 4 spaces; removed UF column to fit line_columns

--- app.py
+++ app.py
@@ -12,10 +12,16 @@ import pandas as pd
 
 filepath = 'modelo_arquivo.txt'
 with open(filepath) as fp:
-   line = fp.readline()
-   cnt = 1
-   while line:
-       print("Line {}: {}".format(cnt, line.strip()))
-       line = fp.readline()
-       # df_header =
-       cnt += 1
+    line = fp.readline()
+    cnt = 1
+    while line:
+        # print(" ISSO EH PRINT ? ")
+        if(int(line[7])==0):
+            line_columns = line.split()
+            df = pd.DataFrame([line_columns], columns=["Nome da Empresa", "Num de Inscrição da Empresa", "Nome do Banco", "Nome da Rua", "Número do Local", "Nome da Cidade", "CEP"])
+            print(df)
+            # print("Line {}: {}".format(cnt, line.strip()))
+            # print(" POS 8 ", line[7], "\n")
+            line = fp.readline()
+            # df_header =
+            cnt += 1
