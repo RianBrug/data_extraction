109f129cabf31d47829867ad54a4aaa997318ef5
rianbrug <riancvb@gmail.com>
2021-06-20 17:21:44 -0300

    info from header lote and archive in dataframe

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ import pandas as pd
 
 filepath = 'modelo_arquivo.txt'
 with open(filepath) as fp:
-    line = fp.readline()
-    line_columns = line.split()
-    cnt = 1
-    while line:
-        register_header = line_columns[0]
-        if(int(register_header[7]) == 0):
-            nameCompany1 = ''.join([i for i in line_columns[2] if not i.isdigit()])
-            nameCompany = [nameCompany1 + " " + line_columns[3]]
+    header_reg = False  # ctrl if df already have register header filepath
+    for line in fp:
+        line = line.split()
+        register_checker = line[0]
+        line_list = register_checker.split()
+        if(int(line_list[0][7]) == 0 and header_reg is False):
+            nameCompany1 = ''.join([i for i in line[2] if not i.isdigit()])
+            nameCompany = [nameCompany1 + " " + line[3]]
             df = pd.DataFrame(
                             columns=["Nome da Empresa",
                             "Num de Inscrição da Empresa", "Nome do Banco",
@@ -28,17 +28,23 @@ with open(filepath) as fp:
                             )
             df["Nome da Empresa"] = nameCompany
 
-        register_lote = line_columns[1]
-        if(int(register_lote[7]) == 1):
+            bank_name = line[4]
+            df["Nome do Banco"] = bank_name
+
+        if(header_reg is True and line_list[0][7] == '1'):
+            register_lote = line[1]
             company_subscription_number = register_lote[1:15]
-            company_subscription_number = company_subscription_number[:2] + \
+            df["Num de Inscrição da Empresa"] = company_subscription_number[:2] + \
                                             "." + company_subscription_number[2:5] + \
                                             "." + company_subscription_number[5:8] + \
                                             "/" + company_subscription_number[8:12] + \
                                             "-" + company_subscription_number[12:15]
-            df["Num de Inscrição da Empresa"] = company_subscription_number
+            df["Nome da Rua"] = line[4] + " " + line[5] + " " + line[6]
+            df["Número do Local"] = line[7]
+            city_name_and_uf = ''.join([i for i in line[8] if not i.isdigit()])
+            cep_number = ''.join([i for i in line[8] if i.isdigit()])
+            df["CEP"] = cep_number[0:5] + "-" + cep_number[5:]
+            df["Nome da Cidade"] = city_name_and_uf[:-2]
+            df["UF"] = city_name_and_uf[-2:]
 
-        bank_name = line_columns[4]
-        df["Nome do Banco"] = bank_name
-        break
-        cnt += 1
+        header_reg = True
