87f13ff60c77c951531a7155a6c9713404346057
rianbrug <riancvb@gmail.com>
2021-06-21 02:19:54 -0300

    data extract to a series; then series append to df_header_lote

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import pandas as pd
 import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
+print("Starting data extraction from 'modelo_arquivo.txt'...")
+
 filepath = 'modelo_arquivo.txt'
 with open(filepath) as fp:
     header_reg = False  # ctrl if df_header_lote already have registeredd header file data
@@ -22,35 +24,40 @@ with open(filepath) as fp:
 
         line_list = line[0].split()
 
-        if(header_reg is False and line_list[0][7] == '0'):
+        if(line_list[0][7] == '0'):
             company_name_1 = ''.join([i for i in line[2] if not i.isdigit()])
-            company_name = [company_name_1 + " " + line[3]]
-            df_header_lote = pd.DataFrame(
-                            columns=["Nome da Empresa",
-                            "Num de Inscrição da Empresa", "Nome do Banco",
-                            "Nome da Rua", "Número do Local",
-                            "Nome da Cidade", "CEP", "UF"],
-                            )
-            df_header_lote["Nome da Empresa"] = company_name
+            company_name = company_name_1 + " " + line[3]
 
+            if(header_reg is False):
+                df_header_lote = pd.DataFrame(
+                                columns=["Nome da Empresa",
+                                "Num de Inscrição da Empresa", "Nome do Banco",
+                                "Nome da Rua", "Número do Local",
+                                "Nome da Cidade", "CEP", "UF"],
+                                )
             bank_name = line[4]
-            df_header_lote["Nome do Banco"] = bank_name
 
         if(header_reg and line_list[0][7] == '1'):
             register_lote = line[1]
             company_subscription_number = register_lote[1:15]
-            df_header_lote["Num de Inscrição da Empresa"] = company_subscription_number[:2] + \
+            company_subscription_number_formated = company_subscription_number[:2] + \
                                             "." + company_subscription_number[2:5] + \
                                             "." + company_subscription_number[5:8] + \
                                             "/" + company_subscription_number[8:12] + \
                                             "-" + company_subscription_number[12:15]
-            df_header_lote["Nome da Rua"] = line[4] + " " + line[5] + " " + line[6]
-            df_header_lote["Número do Local"] = line[7]
+            address = line[4] + " " + line[5] + " " + line[6]
+            address_number = line[7]
             city_name_and_uf = ''.join([i for i in line[8] if not i.isdigit()])
             cep_number = ''.join([i for i in line[8] if i.isdigit()])
-            df_header_lote["CEP"] = cep_number[0:5] + "-" + cep_number[5:]
-            df_header_lote["Nome da Cidade"] = city_name_and_uf[:-2]
-            df_header_lote["UF"] = city_name_and_uf[-2:]
+            cep_number_formated = cep_number[0:5] + "-" + cep_number[5:]
+            city_name = city_name_and_uf[:-2]
+            uf = city_name_and_uf[-2:]
+
+            row = pd.Series([company_name, company_subscription_number_formated,
+                            bank_name, address, address_number, city_name,
+                            cep_number_formated, uf], index=df_header_lote.columns)
+
+            df_header_lote = df_header_lote.append(row, ignore_index=True)
 
         header_reg = True
 
@@ -73,6 +80,24 @@ with open(filepath) as fp:
             price_with_comma = locale.currency(float(payment_date_and_price[8:-2] + "." + payment_date_and_price[-2:]))
             df_detail_seg["Valor do Pagamento"] = price_with_comma
 
-
         header_detail = True
-        import pdb; pdb.set_trace()
+
+
+def main():
+
+    filepath = 'modelo_arquivo.txt'
+    with open(filepath) as fp:
+        header_reg = False  # ctrl if df_header_lote already have registeredd header file data
+        header_detail = False  # II
+
+        for line in fp:
+            line = line.split()
+
+            line_list = line[0].split()
+            import pdb; pdb.set_trace()
+
+            # create series from data
+
+if __name__ == "__main__":
+
+    main()
