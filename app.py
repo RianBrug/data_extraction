cee6adeb8927ef7dc05bd1b0ff7acf82f289b92c
rianbrug <riancvb@gmail.com>
2021-06-21 02:39:29 -0300

    row_details and df_detail_seg append

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ print("Starting data extraction from 'modelo_arquivo.txt'...")
 
 filepath = 'modelo_arquivo.txt'
 with open(filepath) as fp:
-    header_reg = False  # ctrl if df_header_lote already have registeredd header file data
-    header_detail = False  # II
+    ctrl_creation_df_header = False  # ctrl if df_header_lote already have created df
+    ctrl_creation_df_detail = False  # II
 
     for line in fp:
         line = line.split()
@@ -28,7 +28,7 @@ with open(filepath) as fp:
             company_name_1 = ''.join([i for i in line[2] if not i.isdigit()])
             company_name = company_name_1 + " " + line[3]
 
-            if(header_reg is False):
+            if(ctrl_creation_df_header is False):
                 df_header_lote = pd.DataFrame(
                                 columns=["Nome da Empresa",
                                 "Num de Inscrição da Empresa", "Nome do Banco",
@@ -37,7 +37,7 @@ with open(filepath) as fp:
                                 )
             bank_name = line[4]
 
-        if(header_reg and line_list[0][7] == '1'):
+        if(ctrl_creation_df_header and line_list[0][7] == '1'):
             register_lote = line[1]
             company_subscription_number = register_lote[1:15]
             company_subscription_number_formated = company_subscription_number[:2] + \
@@ -59,45 +59,31 @@ with open(filepath) as fp:
 
             df_header_lote = df_header_lote.append(row, ignore_index=True)
 
-        header_reg = True
+        ctrl_creation_df_header = True
 
-        if(header_reg and line_list[0][7] == '3'):
-            # register_detail = line[1]
-            if(header_detail):
-                customer_name_1 = ''.join([i for i in line[0] if not i.isdigit()])
-                customer_name = [customer_name_1[1:] + " " + line[1] + " " + line[2]]
+        if(ctrl_creation_df_header and line_list[0][7] == '3'):
+
+            if(ctrl_creation_df_detail is False):
                 df_detail_seg = pd.DataFrame(
                                 columns=["Nome do Favorecido",
                                 "Data de Pagamento", "Valor do Pagamento",
                                 "Número do Documento Atribuído pela Empresa", "Forma de Lançamento"],
                                 )
-            df_detail_seg["Nome do Favorecido"] = customer_name
+                ctrl_creation_df_detail = True
+
+            customer_name_1 = ''.join([i for i in line[0] if not i.isdigit()])
+            customer_name = [customer_name_1[1:] + " " + line[1] + " " + line[2]]
             payment_date_and_price = ''.join([i for i in line[4] if i.isdigit()])
-            df_detail_seg["Data de Pagamento"] = payment_date_and_price[0:2] + "/" \
-                                                + payment_date_and_price[2:4] \
-                                                + "/" + payment_date_and_price[4:8]
+            payment_date = payment_date_and_price[0:2] + "/" \
+                            + payment_date_and_price[2:4] \
+                            + "/" + payment_date_and_price[4:8]
 
-            price_with_comma = locale.currency(float(payment_date_and_price[8:-2] + "." + payment_date_and_price[-2:]))
-            df_detail_seg["Valor do Pagamento"] = price_with_comma
+            price_with_comma = locale.currency(
+                                float(payment_date_and_price[8:-2] + "." + payment_date_and_price[-2:]))
+            doc_number_attr_by_company = line[3]
+            payment_method = ("Crédito em Conta Corrente" if int(line[6]) == 0
+                                                        else "Crédito em Conta Poupança")
 
-        header_detail = True
-
-
-def main():
-
-    filepath = 'modelo_arquivo.txt'
-    with open(filepath) as fp:
-        header_reg = False  # ctrl if df_header_lote already have registeredd header file data
-        header_detail = False  # II
-
-        for line in fp:
-            line = line.split()
-
-            line_list = line[0].split()
-            import pdb; pdb.set_trace()
-
-            # create series from data
-
-if __name__ == "__main__":
-
-    main()
+            row_details = pd.Series([customer_name, payment_date, price_with_comma,
+                                    doc_number_attr_by_company, payment_method], index=df_detail_seg.columns)
+            df_detail_seg = df_detail_seg.append(row_details, ignore_index=True)
